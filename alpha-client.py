4fa1fc44d25549fcaf9fb6dc0d425a48307a7310
root <root@ip-172-31-36-34.ap-northeast-1.compute.internal>
2020-02-21 18:11:11 +0000

    read auth log file

--- alpha-client.py
+++ alpha-client.py
@@ -2,8 +2,22 @@
 import os
 auth_file='/var/log/auth.log'
 
+def tail_line(authlogfile):
+    authlogfile.seek(0,2)
+    while True:
+        readline=authlogfile.readline()
+        if not readline:
+            continue
+        yield readline
+
 def attempt(auth_file):
     authlogfile=open(auth_file,"r")
-    print(authlogfile.read())
+    #print(dir(authlogfile))
+    #print(authlogfile.read())
+    lines=tail_line(authlogfile)
+    for i in lines:
+        print(i)
+
+
 
 attempt(auth_file)
