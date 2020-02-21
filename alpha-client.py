b7b2ecead8430a8d9c8a9d4c72b2e813ea6a2241
root <root@ip-172-31-36-34.ap-northeast-1.compute.internal>
2020-02-21 20:46:57 +0000

    intialize connection with server

--- alpha-client.py
+++ alpha-client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os,re
+import os,re,socket
 auth_file='/var/log/auth.log'
 
 def tail_line(authlogfile):
@@ -10,15 +10,33 @@ def tail_line(authlogfile):
             continue
         yield readline
 
+
+def report(attempt_count):
+    server = "172.31.45.182"
+    port = 8080
+    
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((server, port))
+        s.sendall(attempt_count.encode('utf-8'))
+        print ("Data sent to server")
+
+    finally:
+        s.close()
+
+
+
+
 def attempt(auth_file):
     authlogfile=open(auth_file,"r")
     #print(dir(authlogfile))
     #print(authlogfile.read())
     lines=tail_line(authlogfile)
     for i in lines:
-        #print(i)
+        print(i)
         if re.match('.*sshd.*?(Invalid\suser|Accepted).*',i) is not None:
             print("attempt 1")
+            report('1')
 
 
 
